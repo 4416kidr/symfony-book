b09733ffe40412da712a51d31f79014ab6cc4571
TOFU <4416kidr@gmail.com>
2023-10-23 16:41:46 +0900

    finish step-10

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -29,9 +29,9 @@ class CommentRepository extends ServiceEntityRepository
     {
         $query = $this->createQueryBuilder('c')
             ->andWhere('c.conference = :conference')
-            ->setParameter('conference', '$conference')
+            ->setParameter('conference', $conference)
             ->orderBy('c.createdAt', 'DESC')
-            ->setMaxResults('self::PAGINATOR_PER_PAGE')
+            ->setMaxResults(self::PAGINATOR_PER_PAGE)
             ->setFirstResult($offset)
             ->getQuery()
         ;
