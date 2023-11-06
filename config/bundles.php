f9abc846e76953f0e4784b8a840a51e70ef8ff14
TOFU <4416kidr@gmail.com>
2023-11-06 18:04:18 +0900

    symfony composer req encore composer require symfony/stimulus-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
 ];
